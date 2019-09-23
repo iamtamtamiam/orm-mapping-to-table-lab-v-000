1b335291c06d034e7dcc0c9b2a7c74f1abc6e557
Tam Vu <vutam_a@hotmail.com>
2019-09-23 01:45:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
   
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE IF NOT EXISTS students (
+      DROP TABLE IF EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
