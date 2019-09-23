6cf4faef49ad26d7402da57cabeeed580235db57
Tam Vu <vutam_a@hotmail.com>
2019-09-23 01:59:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,10 +41,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
-  def self.create(name, grade)
-    student = Student.new(name, grade)
+  def self.create(name, album)
+    student = Student.new(name, album)
     student.save
-    student 
-  end 
+    song
+  end
   
 end
